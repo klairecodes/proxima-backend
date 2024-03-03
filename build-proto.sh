da16aa6af001d7318d95cc60fa2f97a115ec2d09
Klaire Curde <klairecodes@gmail.com>
2024-03-02 20:21:38 -0500

    protobuf updates, python client test started

--- build-proto.sh
+++ build-proto.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-protoc -I=./googleapis/google -I=./ --java_out=./src --kotlin_out=./src protos/proxima.proto
+#protoc -I=./googleapis/google -I=./ --java_out=./src --kotlin_out=./src protos/proxima.proto
+python -m grpc_tools.protoc -I=./googleapis/google/ -I./ --python_out=./src protos/proxima.proto
---
+++ src/client.py
@@ -0,0 +1,7 @@
+import protos.proxima_pb2 as proxima
+
+class ProximaServicer(proxima.Proxima):
+
+
+def GetUser(self, request, context):
+
